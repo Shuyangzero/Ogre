f65de1961a175b7d0e9c62dbc6217e670ad7517e
CMUShuyang <shuyang1@andrew.cmu.edu>
2020-01-05 14:25:16 -0500

    update

--- example/example.py
+++ example/example.py
@@ -6,16 +6,14 @@ sys.path.append('../')
 import slab_generator
 from pymatgen.io.vasp.inputs import Poscar
 
-struct = './PENCEN.POSCAR.vasp'
+struct = '~/Downloads/aspirin_form_1.POSCAR.vasp'
 struct_ase = read(struct)
-miller_index = [1, 1, 2]
-layers = 3
+miller_index = [1, 0, 0]
+layers = 2
 vacuum = 15
 working_dir = os.path.abspath('.')
 slab_list = slab_generator.repair_organic_slab_generator_move(struct_ase,
                                                               miller_index,
                                                               layers, vacuum,
-                                                              working_dir, [3,
-                                                                            3,
-                                                                            1])
+                                                              working_dir, None)
 Poscar(slab_list[0]).write_file("Ogre_Surface.POSCAR.vasp")
