155a0242d6885c767e0feb48d65b826a3bde87d0
CMUShuyang <shuyang1@andrew.cmu.edu>
2020-01-05 14:47:32 -0500

    update

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-    name='ogreswamp',
+    name='ogre',
     version='0.0',
-    packages=['ogreswamp',
+    packages=['ogre',
               ],
-    #find_packages(exclude=[]),
-    install_requires=['numpy', 'matplotlib', 'pymatgen==2019.6.5', 'networkx==2.3','tqdm',
+    # find_packages(exclude=[]),
+    install_requires=['numpy', 'matplotlib', 'pymatgen==2019.6.5',
+                      'networkx==2.3', 'tqdm',
                       "ase @ https://gitlab.com/ase/tarball/master"],
 )
