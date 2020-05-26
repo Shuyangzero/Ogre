6bdaf3a9a9ea8a6863a6db4a83b4af77293c43fc
manny405 <ibier@andrew.cmu.edu>
2020-05-26 00:28:39 -0400

    Finished multiplot version of convergence plots

--- example/SWAMP/OgreSWAMP_Analysis/surface_energy.py
+++ example/SWAMP/OgreSWAMP_Analysis/surface_energy.py
@@ -6,11 +6,12 @@
 convergence_plots_kw = \
 {
     "structure_name": "ASPIRIN",
-    "scf_path": "SCF",
+    "scf_path": "SCF_12",
     "threshold":  1,
     ## MAYBE MAX_LAYERS INSTEAD
     "max_layers": -1,
     "fontsize": 16,
+    "combined_figure": True
 }
 
 convergence_plots(**convergence_plots_kw)
